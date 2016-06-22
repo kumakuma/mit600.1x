fcff2ed0679705600540a52932d50658158081fa
kumakuma <dan.smith.me@gmail.com>
2016-06-22 20:47:57 +0100

    loops, updated

--- lec3.py
+++ lec3.py
@@ -63,4 +63,59 @@
 #     print('Cube root of ' + str(x) + ' is ' + str(ans))
 
 #
-# ----- 
+# ----- L3 P3 -----
+#
+#
+#
+# school = 'Massachusetts Institute of Technology'
+# numVowels = 0
+# numCons = 0
+#
+# for char in school:
+#     if char == 'a' or char == 'e' or char == 'i' \
+#        or char == 'o' or char == 'u':
+#         numVowels += 1
+#     elif char == 'o' or char == 'M':
+#         print char
+#     else:
+#         numCons -= 1
+#
+# print 'numVowels is: ' + str(numVowels)
+# print 'numCons is: ' + str(numCons)
+
+#
+# greeting = 'Hello!'
+# count = 0
+#
+# for letter in greeting:
+#     count += 1
+#     if count % 2 == 0:
+#         print letter
+#     print letter
+# print 'done'
+
+# num = 10
+# for num in range(5):
+#     print num
+# print num
+
+
+# for v in range(20):
+#     if v % 4 == 0:
+#         print v
+#     if v % 16 == 0:
+#         print str(v) + 'Foo!'
+
+# ----- L3 p5a -----#
+# for i in range(0, 10, 2):
+#    print(str(i + 2))
+# print "Goodbye!"
+
+# print("Hello!")
+# for i in range(10, 0, -2):
+#     print(str(i))
+
+total = 0
+for i in range(end + 1):
+    total = total + i
+print(str(total))
