056c0bc3bce9f9d99b446ab792ac1adc4b0f666d
kumakuma <dan.smith.me@gmail.com>
2016-06-23 19:48:36 +0100

    lec4 finished

--- lec4_functions.py
+++ lec4_functions.py
@@ -1,14 +1,14 @@
 # functions in python
 
 
-def iterative_power(x, p):
+def iterative_power(x, n):
     """
     :param x: int or float
-    :param p: int or float - the power we want to raise x to
-    :return: result of x ^ p
+    :param n: int or float - the power we want to raise x to
+    :return: result of x ^ n
     """
     result = 1
-    for i in range(p):
+    for i in range(n):
         result = result * x
         print('Iteration: ' + str(i + 1) + ' current result: ' + str(result))
     return result
@@ -22,13 +22,100 @@ def square(x):
     return x * x
 
 
-def evalQuadratic(a, b, c, x):
+def two_power(x, n):
     """
-    :param a, b, c: numerical values for the coefficients of a quadratic equation
-    :param x: numerical value at which to evaluate the quadratic
-    :return: the value of the quadratic
+    :param x: int or float
+    :param n: int or float - the power where n is a power of 2
+    :return: the result of x ^ n
     """
+    while n > 1:
+        x = square(x)
+        n = n/2
+    return x
 
-    value = (a * x*x) + (b * x) + c
+# L4 problem 5
+def clip(lo, x, hi):
+    """
+    Takes in three numbers and returns a value based on the value of x.
+    Returns:
+     - lo, when x < lo
+     - hi, when x > hi
+     - x, otherwise
+    """
+    return min(max(lo, x), hi)
 
-    return value
+
+# bi-sectional root finder
+def find_root(x, power, epsilon):
+    """
+    :param x: int or float
+    :param power: int, >= 1
+    :param epsilon: int or float > 0
+    :return: float such that y**power is within epsilon of x
+            if no such float exists it returns None
+    """
+    # can't find even powered root of negative number so avoid it
+    if x < 0 and power % 2 ==0:
+        return None
+    low = min(-1, x)  # keeps correct min even if negative or fractional
+    high = max(1, x)
+    ans = (high + low) / 2.0
+    while abs(ans ** power - x) > epsilon:
+        if ans ** power < x:
+            low = ans
+        else:
+            high = ans
+        ans = (high + low) / 2.0
+    return ans
+
+
+# ----- L4 P8 -----
+#  fourth power
+def fourth_power(x):
+    """
+    :param x: int or float
+    :return: int or float - x raised to the fourth power
+    """
+    return square(square(x))
+
+
+# ----- L4 P9 -----
+def odd(x):
+    """
+    :param x: int or float
+    :return: True if x is odd, False otherwise
+    """
+    return x % 2 != 0
+
+
+# ----- L4 P10 -----
+def is_vowel(char):
+    """
+    char: a single letter of any case
+
+    returns: True if char is a vowel and False otherwise.
+    """
+    result = False
+    vowels = 'aeiouAEIOU'
+    x = 0
+    while x < len(vowels):
+        if char == vowels[x]:
+            result = True
+            break
+        x += 1
+    return result
+
+
+#  ----- L4 P11 -----
+def is_vowel_2(char):
+    """
+    char: a single letter of any case
+
+    returns: True if char is a vowel and False otherwise.
+    """
+    # Your code here
+    vowels = 'aeiouAEIOU'
+    for v in vowels:
+        if char == v:
+            return True
+    return False
