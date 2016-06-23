7627e505aa170ad55e94ca74dbdde7291b4312a9
kumakuma <dan.smith.me@gmail.com>
2016-06-23 08:49:19 +0100

    finished lec3

--- lec3.py
+++ lec3.py
@@ -65,8 +65,6 @@
 #
 # ----- L3 P3 -----
 #
-#
-#
 # school = 'Massachusetts Institute of Technology'
 # numVowels = 0
 # numCons = 0
@@ -115,7 +113,93 @@
 # for i in range(10, 0, -2):
 #     print(str(i))
 
-total = 0
-for i in range(end + 1):
-    total = total + i
-print(str(total))
+# total = 0
+# for i in range(end + 1):
+#     total = total + i
+# print(str(total))
+
+
+# ----- program to convert to binary -----
+# #
+# num = int(raw_input("enter a decimal number to convert: "))
+# if num < 0:
+#     isNeg = True
+#     num = abs(num)
+# else:
+#     isNeg = False
+# result = ''
+# if num == 0:
+#     result = '0'
+# while num > 0:
+#     result = str(num % 2) + result
+#     num = num / 2
+# if isNeg:
+#     result = '-' + result
+# print(result)
+
+# ----- approximation methods -----
+# x = 25
+# epsilon = 0.01
+# step = epsilon**2
+# numGuesses = 0
+# ans = 0.0
+# while (abs(ans**2 - x)) >= epsilon and ans <= x:
+#     ans += step
+#     numGuesses += 1
+# print('numGuesses = ' + str(numGuesses))
+# if abs(ans**2 - x) >= epsilon:
+#     print('failed on square root of ' +str(x))
+# else:
+#     print(str(ans) + ' is close to the square root of ' + str(x))
+
+# ----- BISECTION SEARCH -----
+# guess middle 0 to x, and then make guess smaller or larger as necessary.
+# x = 25
+# epsilon = 0.01
+# numGuesses = 0
+# low = 0.0
+# high = x
+# ans = (high + low)/2.0
+# while abs(ans**2 - x) >=epsilon:
+#     print('low: ' + str(low) + ' high: ' + str(high) + ' ans: ' + str(ans))
+#     numGuesses += 1
+#     if ans**2 < x:
+#         low = ans
+#     else:
+#         high = ans
+#     ans = (high + low)/2.0
+# print('numGuesses = ' + str(numGuesses))
+# print(str(ans) + ' is close to the square root of ' + str(x))
+
+# ----- l3 P9 -----
+# Guess the secret number 0 - 100
+# high = 100
+# low = 0
+# guess = (high + low) / 2
+# reply = ''
+# prompt = "Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter'c' to indicate I guessed correctly.  "
+# print('Please think of a number between 0 and 100!')
+# while reply != 'c':
+#     reply = str(raw_input('Is your secret number ' + str(guess) + '?\n' + prompt))
+#     if reply == 'h':
+#         high = guess
+#     elif reply == 'l':
+#         low = guess
+#     elif reply == 'c':
+#         break
+#     else:
+#         print"Sorry I did not understand your input."
+#     guess = (high + low) / 2
+# print("Game over your secret number was: " + str(guess))
+
+# ----- NEWTON RAPHSON Root Finding -----
+#
+#  finds the root of a polynomial ( g - p(g)/p'(g) )
+#
+# epsilon = 0.01
+# y = 24.0
+# guess = y/2.0
+#
+# while abs(guess*guess - y) >= epsilon:
+#     guess = guess - (((guess**2) - y) / (2*guess))
+# print('Square root of ' + str(y) + ' is about ' + str(guess))
